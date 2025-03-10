5b0829cc8657ad092ca8a987652fa079d9f75bed
wals <serban.ionut.valentin2003@gmail.com>
2025-03-10 21:59:39 +0200

    Modified download file

--- datasets/download_dataset.py
+++ datasets/download_dataset.py
@@ -6,3 +6,7 @@ path = kagglehub.dataset_download("mohnishsaiprasad/forest-fire-images")
 print("Path to dataset files:", path)
 
 
+# Download latest version
+path = kagglehub.dataset_download("elmadafri/the-wildfire-dataset")
+
+print("Path to dataset files:", path)
